6b460a58c4edf31a6d2ca3d2a0caf62a1f1174d4
officialravi2222 <officialravi2222@gmail.com>
2023-12-19 13:20:17 +0530

    finished

--- index.js
+++ index.js
@@ -4,15 +4,109 @@ const button = document.querySelector("button");
 // console.log(div);
 
 
-div.addEventListener("click",(e) => (
-    console.log("div")
-),true)
+// div.addEventListener("click",(e) => (
+//     console.log("div")
+// ))
 
 
-button.addEventListener("click",(e) => (
-    console.log("button")
-),true)
+// button.addEventListener("click",(e) => (
+//     console.log("button")
+// ))
 
 // This is bubbling
+// OUTPUT :
+// button
+// div
+
+
+/*  Event CAPTURING  */
+
+
+// div.addEventListener("click",(e) => (
+//     console.log("div")
+// ),true)
+
+
+// button.addEventListener("click",(e) => (
+//     console.log("button")
+// ),true)
+
+// This is bubbling
+// OUTPUT :
+//div
+// button
+
+
+// Event PROPOGATION
+
+
+
+// div.addEventListener("click",() => (
+//     console.log("div")
+// ))
+
+
+// button.addEventListener("click",(event) => {  
+
+//     event.stopPropagation()
+//     console.log("button")
+   
+    
+// })
+
+// This is bubbling
+// OUTPUT :
+// button
+// Here it is not going to its parent 
+
+
+
+// IMMMEDIATE STOP PROPOGATION
+
+// CASE 1:
+
+// button.addEventListener("click",(event) => {  
+//     console.log("button")
+// })
+
+
+// button.addEventListener("click",(event) => {  
+//     console.log("button1")
+// })
+
+// This is bubbling
+// OUTPUT :
+// button
+// button1
+
+
+// CASE 2:
+
+// button.addEventListener("click",(event) => {  
+
+//     event.stopImmediatePropagation()
+//     console.log("button")
+// })
+
+
+// button.addEventListener("click",(event) => {  
+
+//     console.log("button1")
+// })
+
+// This is bubbling
+// OUTPUT :
+// button
+
+
+// PREVENT DEFAULt
+
+const anchor = document.querySelector("a")
+
+console.log(anchor);
+
+anchor.addEventListener("click", function(event){
+    event.preventDefault()
+})
 
 
